81312e39c49072ac7c59637e5f67273c76412455
Jibidus <jibidus@gmail.com>
2016-12-06 17:13:07 +0100

    Fix instrumented test

--- android-app/src/androidTest/java/com/sogilis/example/android/app/MainActivityTest.java
+++ android-app/src/androidTest/java/com/sogilis/example/android/app/MainActivityTest.java
@@ -3,7 +3,9 @@ package com.sogilis.example.android.app;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.WindowManager;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +23,19 @@ public class MainActivityTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void unlockScreen() {
+        final MainActivity activity = mActivityTestRule.getActivity();
+        Runnable wakeUpDevice = new Runnable() {
+            public void run() {
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        };
+        activity.runOnUiThread(wakeUpDevice);
+    }
 
     @Test
     public void mainActivityTest() {
